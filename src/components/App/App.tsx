482fa8d14604637a4de9e618df902c3f3518a753
Daniel Gomes <danielgomes@dginfope.com.br>
2020-11-07 14:08:19 -0300

    add header components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from '../Header';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header title="AlgaStok"/>
     </div>
   );
 }
