9dd2774cc06503fd0b824c8447aa71ecfcb8112d
Daniel Gomes <danielgomes@dginfope.com.br>
2020-11-07 15:05:35 -0300

    add button component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from '../Button';
 import Header from '../Header';
 import './App.css';
+
+function TestComponent() {
+  return <img width="16" src="http://sistemas.tjam.jus.br/coij/wp-content/uploads/2018/04/1-foto.png" alt="coll"></img>
+}
 
 function App() {
   return (
     <div className="App">
-      <Header title="AlgaStok"/>
+      <Header title="AlgaStok" />
+      <div className="container">
+        <Button
+          onClick={() => window.alert('Alert')}
+          appendIcon={<TestComponent />}>
+          Alert
+        </Button>
+      </div>
     </div>
   );
 }
