bf1f50afc44ca1ed041b94af082aaa8f3e021d61
Daniel Gomes <danielgomes@dginfope.com.br>
2020-11-10 17:28:15 -0300

    partial

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -5,16 +5,53 @@ import Products from './Table.mockdata';
 const headers = [
     { key: 'name', value: 'Product' },
     { key: 'price', value: 'Price' },
+    { key: 'actions', value: 'Actions' },
     { key: 'stock', value: 'Availble Stock' }
 ]
 
+declare interface TableHeader {
+    key: string
+    value: string
+    right?: boolean
+}
+
+type IndexedHeaders = {
+    [key: string]: TableHeader
+}
+
+type OrganizedItem = {
+    [key: string]: any
+}
+
+function organizeData(data: [], headers: TableHeader[]) {
+    const indexedHeaders: IndexedHeaders = {}
+
+    headers.forEach(header => {
+        indexedHeaders[header.key] = {
+            ...header
+        }
+    })
+
+    const headerKeysInOrder = Object.keys(indexedHeaders)
+
+    const organizedData = data.map(item => {
+        const organizedItem: OrganizedItem = {}
+
+        headerKeysInOrder.forEach(key => {
+            organizedItem[key] = item[key]
+        })
+    })
+}
+
+
 const Table: React.FC = () => {
+    organizeData([], headers);
     return <table className="AppTable">
         <thead>
             <tr>
                 {
                     headers.map(header =>
-                        <th key="header.key">
+                        <th key={header.key}>
                             {header.value}
                         </th>)
                 }
