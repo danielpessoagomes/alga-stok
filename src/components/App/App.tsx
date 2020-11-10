b54d9d93306c18c411c53c89eb26fca9ee6220cd
Daniel Gomes <danielgomes@dginfope.com.br>
2020-11-09 22:35:13 -0300

    clear app + add table component's base files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 
 
 function App() {
-
-  
-
   return (
     <div className="App">
       <Header title="AlgaStok" />
       <Container>
-        <ul>
-      {
-        ['Daniel', 'Heitor', 'Jessica', 'Daniel'].map((name, index)=> {
-          return <li key={index}>{name}</li>
-        })
-      }   
-        </ul>
+
       </Container>
     </div>
   );
