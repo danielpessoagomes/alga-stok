0501c1894ccef83d976f913537370137f9f89740
Daniel Gomes <danielgomes@dginfope.com.br>
2020-11-13 18:12:45 -0300

    mock server

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import http from './utils/http';
-
-http.get('/posts');
-http.get('https://viacep.com.br/ws/01001000/json/')
 
 ReactDOM.render(
   <React.StrictMode>
